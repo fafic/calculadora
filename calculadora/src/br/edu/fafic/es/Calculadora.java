74cc96c45a48a130bdc4400daeda88814705c066
André Roberto <andre.robertolins2018@gmail.com>
2020-10-01 19:57:24 -0300

    André - Método Subtrair

--- calculadora/src/br/edu/fafic/es/Calculadora.java
+++ calculadora/src/br/edu/fafic/es/Calculadora.java
@@ -2,5 +2,10 @@ package br.edu.fafic.es;
 
 public class Calculadora {
 
+	//André - Método Subtração
+	public float subtrair(float a, float b) {
+		float resultado = a-b;
+		return resultado;
+	}
     
 }
